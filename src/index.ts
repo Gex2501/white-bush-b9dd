14740b36894dbc796af7324c3c88172c4bf39dfa
Gex2501 <greg.estes@gmail.com>
2025-04-03 01:47:48 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk dog",
+      prompt: "cyberpunk dog white labrador",
     };
 
     const response = await env.AI.run(
